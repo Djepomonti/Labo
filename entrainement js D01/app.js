52dc1add3ec3d24b3c60cb60f0bc614be9ac0b47
Jpomonti <jerome.pomonti@gmail.com>
2022-04-21 15:18:50 +0200

    ex JS 1

--- entrainement js D01/app.js
+++ entrainement js D01/app.js
@@ -1,7 +1,50 @@
-let age = 100;
+let pizza = "reine";
+let nombre = 10;
+let valeur;
+let remise;
+let pluriel;
+let affRemise;
 
-if (age > 90) {
-    alert(age)
-
+if (pizza == "calzone") {
+    valeur = 8;
+}
+else if (pizza == "reine") {
+    valeur = 10;
 
 }
+else if (pizza == "royale") {
+    valeur = 12;
+}
+
+if ((nombre >= 5) && (nombre < 10)) {
+    remise = 2;
+
+} else if (nombre >= 10) {
+    remise = 5;
+
+} else {
+    remise = 0;
+}
+if (nombre < 2) {
+    pluriel = "pizza";
+
+} else {
+    pluriel = "pizzas";
+}
+if (remise == 0) {
+    affRemise = "";
+
+} else {
+    affRemise = `Remise: ${remise} &euro;<br>`;
+}
+
+
+let total = nombre * valeur - remise;
+
+document.querySelector(".test").innerHTML =
+    `Vous avez commandé ${nombre} ${pluriel}<br>
+Type de pizza commandée: ${pizza}<br>
+Valeur unitaire: ${valeur}&euro;<br>
+${affRemise}
+Total a payer: ${total}&euro;
+`
